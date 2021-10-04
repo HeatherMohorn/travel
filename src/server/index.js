70112c220b102e6c27d98394a8073e664a639cb0
Heather Mohorn <heather.mohorn@gmail.com>
2021-10-03 20:23:04 -0400

    Service workers

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,11 @@ const express = require('express');
 //Start up an instance of an app
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 /* Middleware */
 //Configuring express to use body-bodyParser
 const bodyParser = require('body-parser');
